833bf317279df58be909215a3647c5321d14095a
Maxim Indykov <indmaksim@gmail.com>
2015-11-14 16:04:16 +0300

    Добавлен js входа

---
+++ layout/js/index.js
@@ -0,0 +1,7 @@
+$(function() {
+  //Клик на кнопку Войти в хедере
+  $('#login_btn').on('click', function() {
+    //сюда вставить анимацию формы входа
+    $('.color_overlay').css('opacity', 1)
+  });
+});
