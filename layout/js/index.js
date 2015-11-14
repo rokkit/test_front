ccaaead586ea813fe2f1f9ce1153bd927dcd9784
Your Name <you@example.com>
2015-11-14 16:11:44 +0300

    Добавлкны моки анимации

--- layout/js/index.js
+++ layout/js/index.js
@@ -3,5 +3,9 @@ $(function() {
   $('#login_btn').on('click', function() {
     //сюда вставить анимацию формы входа
     $('.color_overlay').css('opacity', 1)
+    $('.color_overlay').css('opacity', 1) //Клик через колор оверлей
+    $('.color_overlay').css('opacity', 1) //Блюр заднего фона / Скейл заднего фона
+    $('.color_overlay').css('opacity', 1) //Движение окна вправо
+
   });
 });
