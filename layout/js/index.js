01314ba34b4cd0fee51756c10a5b07f85a9625a1
Your Name <you@example.com>
2015-11-14 18:02:23 +0300

    Добавлена анимация логина

--- layout/js/index.js
+++ layout/js/index.js
@@ -2,10 +2,11 @@ $(function() {
   //Клик на кнопку Войти в хедере
   $('#login_btn').on('click', function() {
     //сюда вставить анимацию формы входа
-    $('.color_overlay').css('opacity', 1)
-    $('.color_overlay').css('opacity', 1) //Клик через колор оверлей
-    $('.color_overlay').css('opacity', 1) //Блюр заднего фона / Скейл заднего фона
-    $('.color_overlay').css('opacity', 1) //Движение окна вправо
-
+	var login_form = document.getElementById("login_form")
+	var color_overlay = document.getElementById("color_overlay")
+	var main = document.getElementById('main_content')
+  	TweenLite.to(login_form, 1, {left:"0"})
+  	TweenLite.to(color_overlay, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
+  	TweenLite.to(main_content, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
   });
 });
