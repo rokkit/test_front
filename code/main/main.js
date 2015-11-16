f4ed9fc4497700f75d6258cf3aa11ad5fd0580c5
Pavel Tretyakov <tretyakovpavel.ru@gmail.com>
2015-11-16 17:30:02 +0300

    Created auth object (tmp version)

--- code/main/main.js
+++ code/main/main.js
@@ -1,9 +1,7 @@
 'use strict';
 
-import $ from 'jquery';
 import _ from 'lodash';
 import 'rivets-backbone-adapter';
-import 'jquery-mask-plugin';
 import 'backbone';
 import 'gsap';
 
@@ -14,7 +12,8 @@ import Collection 	from 'collections/lounges'		;
 import Template		from 'main/main.hbs'			;
 import ItemTemplate	from 'main/main_itemview.hbs'	;
 
-import AmBehavior from 'behaviors/am/am';
+import BehaviorAm 	from 'behaviors/am/am';
+import ObjectAuth	from 'objects/auth.object';
 
 let offline_data = [{"id":4,"title":"Unity Hall","city_id":4,"color":"#da6f50","blazon":"public/images/blazons/gerb_kazan_unityhall.svg"},{"id":6,"title":"Облака","city_id":3,"color":"#64B6DC","blazon":"public/images/blazons/gerb_spb_liberty.svg"},{"id":5,"title":"Резерв","city_id":2,"color":"#7AC36C","blazon":"public/images/blazons/gerb_spb_blazon.svg"},{"id":1,"title":"Академия","city_id":5,"color":"#7946D6","blazon":"public/images/blazons/gerb_spb_academy.svg"},{"id":2,"title":"Либерти","city_id":1,"color":"#6CB9DD","blazon":"public/images/blazons/gerb_spb_liberty.svg"}]
 
@@ -33,10 +32,9 @@ export default Marionette.CompositeView.extend({
 	el 					: 'body'				 	,
 	template 			: Template 		 	 	 	,
 	childView 			: childView 			 	,
-	childViewContainer 	: '#lounges-body'	 	 	,
-	hostUrl				: 'http://192.168.1.39:82' 	,
+	childViewContainer 	: '#lounges-body'	 	 	,	
 
-	behaviors : { am : { behaviorClass : AmBehavior } } ,
+	behaviors : { am : { behaviorClass : BehaviorAm } } ,
 	ui 		: { 
 		'formLogin'		: '#login_form' 				,
 		'formSign' 		: '#signin_form' 				,
@@ -47,10 +45,11 @@ export default Marionette.CompositeView.extend({
 	events 	: { 'submit @ui.formLogin' 	: 'doLogin' 	, 'submit @ui.formSign' : 'doSign' } ,
 
 	initialize() {
+		this.ObjectAuth = new ObjectAuth();
 		this.collection = new Collection();
-		window.col 		= this.collection;
-		console.log('Init main js' , AmBehavior );
+		
 		this.collection.fetch();
+		
 		return this.on( 'render' , this.afterRender , this );
 	}  ,
 	
@@ -78,66 +77,7 @@ export default Marionette.CompositeView.extend({
 		});
 	} ,
 
-	successAuth(resp) {
-		window.currentUser = resp
-		document.location.href = '/dashboard.html'
-	} ,
-
-	doSign( e ){
-		e.preventDefault()
-		var phone 		= $('#signin_form input[name="phone"]').val().replace('+', '')
-		var password 	= $('#signin_form input[name="password"]').val()
-		var name 		= $('#signin_form input[name="name"]').val()
-		$.post(this.hostUrl + '/api/v1/auth/registrations.json', {
-			phone 	: phone,
-			name 	: name,
-			password 	: password
-		}, function(resp) {
-			if(!resp['errors']) {
-				doLogin(phone, password)
-			} else {
-				$('#signin_form input').removeClass('wrong')
-				if(resp['errors']['name']) {
-					$('#signin_form input[name="name"]').addClass('wrong')
-				}
-				if(resp['errors']['phone']) {
-					$('#signin_form input[name="phone"]').addClass('wrong')
-				}
-				if(resp['errors']['password']) {
-					$('#signin_form input[name="password"]').addClass('wrong')
-				}
-			}
-		});
-	} ,
-
-	doLogin( event ){
-		var phone 		= this.ui.inputPhone.val() 		;
-		var password 	= this.ui.inputPassword.val()	;
-
-		$.post(this.hostUrl + '/api/v1/auth/sessions.json', {
-			phone 	: phone ,
-			password: password
-		}, function(resp) {
-			if(!resp['errors']) {
-				console.error('Auth error');
-				this.successAuth(resp);
-			} else {
-				console.info('Success auth');
-				$('#login_form input').removeClass('wrong');
-				
-				if(resp['errors']['phone']) {
-					console.error('Phone error');
-					$('#login_form input[name="phone"]').addClass('wrong');
-				}
-				
-				if(resp['errors']['password']) {
-					console.error('Password error');
-					$('#login_form input[name="password"]').addClass('wrong');
-				}
-			}
-		});
-
-		event.preventDefault();
-	}
+	doSign ( event ) { this.ObjectAuth.doSign( event  );	} ,
+	doLogin( event ) { this.ObjectAuth.doLogin( event ); 	}
 
 });
