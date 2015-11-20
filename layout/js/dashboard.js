ed1035522a3103eb1d1540fa75cb1d003229559d
Maxim Indykov <indmaksim@gmail.com>
2015-11-20 18:50:34 +0300

    Сделаны страницы клиентов и анимации возвращения форм

--- layout/js/dashboard.js
+++ layout/js/dashboard.js
@@ -57,7 +57,7 @@ $(function() {
 
 	$.getJSON(window.hostUrl + '/api/v1/achievements.json', {auth_token: currentUser.auth_token}, function(json) {
 		$('#achievements').empty()
-    // $('#all_ach .wrapper_for_ach').empty()
+    $('#all_ach .wrapper_for_ach').empty()
 		$('#dashboard_ach_btn').text(0+'/'+json.length)
     $.each(json, function(i) {
       var template = "<figure><img class='achievments_icon' src='"+window.hostUrl+this.image+"'><ficapation><h6>"+this.name+"</h6><p>21.09.15</p></ficapation></figure>";
@@ -81,4 +81,71 @@ $(function() {
 					$('#skills').append(template)
 				})
 		})
+
+    // Анимациии Dashboard
+    //
+
+    //Открытие всех достижений
+    $('#dashboard_ach_btn').on('click', function() {
+      animateVertical_popup('all_ach')
+    });
+
+
+    // Открытие всех навыков
+    $('#dashboard_talents_btn').on('click', function() {
+
+      animateVertical_popup('all_talents')
+    });
+    // Открытие достижения
+    $(document).on('click', '#achievements figure',function() {
+      var achiv = $(this)
+      $('#achivka h2').text(achiv.find('h6').text())
+      $('#achivka img').attr('src', (achiv.find('img').attr('src')))
+      animateOverall_popup('achivka')
+    });
 });
+
+// PRELOADER
+$(function() {
+  var html_body = document.getElementById("main_content")
+  TweenLite.to(html_body, 1, {opacity:1})
+})
+
+function animateVertical_popup(el) {
+  var all_ach = document.getElementById(el)
+  var html_body = document.getElementById("html_body")
+  var color_overlay = document.getElementById("color_overlay")
+  var main = document.getElementById('main_content')
+  var wrapper = $('#all_ach_wrapper')[0]
+
+  var tl = null
+
+  var tw1 = TweenLite.to(all_ach, 1, {top:"80px", onComplete: function() {
+    $('body').on('click', function(e) {
+      console.log('ss')
+      tl.reverse()
+    });
+  }})
+  var tw2 = TweenLite.to(color_overlay, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
+  var tw3 = TweenLite.to(main_content, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
+  var tw4 = TweenLite.to(html_body, 1, {overflow:"hidden"})
+  // var tw4 = TweenLite.to(wrapper, 1, {'pointer-events':"auto"})
+  tl = new TimelineLite().add([tw1,tw2,tw3, tw4], 'sequence');
+}
+
+function animateOverall_popup(el) {
+  var achivka = document.getElementById(el)
+  var html_body = document.getElementById("html_body")
+  var color_overlay = document.getElementById("color_overlay")
+  var main = document.getElementById('main_content')
+  var tl = null
+  var tw1 = TweenLite.to(achivka, 0.5, {top:"0", onComplete: function() {
+    $('body').on('click', function(e) {
+      tl.reverse()
+    });
+  }})
+  var tw2 = TweenLite.to(color_overlay, 0.5, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
+  var tw3 = TweenLite.to(main_content, 0.5, {filter:"blur(5px)", "-webkit-filter":"blur(4px)"})
+  var tw4 = TweenLite.to(html_body, 0.5, {overflow:"hidden"})
+  tl = new TimelineLite().add([tw1,tw2,tw3,tw4], 'sequence');
+}
--- layout/js/index.js
+++ layout/js/index.js
@@ -6,68 +6,11 @@ $(function() {
     document.location.href="/pages_lounges_liberty.html"
   });
 
-  // Анимациии Dashboard
-  //
 
-  //Открытие всех достижений
-  $('#dashboard_ach_btn').on('click', function() {
-    animateVertical_popup('all_ach')
-  });
-
-
-  // Открытие всех навыков
-  $('#dashboard_talents_btn').on('click', function() {
-
-    animateVertical_popup('all_talents')
-  });
-  // Открытие достижения
-  $(document).on('click', '#achievements figure',function() {
-    var achiv = $(this)
-    $('#achivka h2').text(achiv.find('h6').text())
-    $('#achivka img').attr('src', (achiv.find('img').attr('src')))
-    animateOverall_popup('achivka')
-  });
 });
 
 
-function animateVertical_popup(el) {
-  var all_ach = document.getElementById(el)
-  var html_body = document.getElementById("html_body")
-  var color_overlay = document.getElementById("color_overlay")
-  var main = document.getElementById('main_content')
-  var wrapper = $('#all_ach_wrapper')[0]
 
-  var tl = null
-
-  var tw1 = TweenLite.to(all_ach, 1, {top:"80px", onComplete: function() {
-    $('body').on('click', function(e) {
-      console.log('ss')
-      tl.reverse()
-    });
-  }})
-  var tw2 = TweenLite.to(color_overlay, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
-  var tw3 = TweenLite.to(main_content, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
-  var tw4 = TweenLite.to(html_body, 1, {overflow:"hidden"})
-  // var tw4 = TweenLite.to(wrapper, 1, {'pointer-events':"auto"})
-  tl = new TimelineLite().add([tw1,tw2,tw3], 'sequence');
-}
-
-function animateOverall_popup(el) {
-  var achivka = document.getElementById(el)
-  var html_body = document.getElementById("html_body")
-  var color_overlay = document.getElementById("color_overlay")
-  var main = document.getElementById('main_content')
-  var tl = null
-  var tw1 = TweenLite.to(achivka, 0.5, {top:"0", onComplete: function() {
-    $('body').on('click', function(e) {
-      tl.reverse()
-    });
-  }})
-  var tw2 = TweenLite.to(color_overlay, 0.5, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
-  var tw3 = TweenLite.to(main_content, 0.5, {filter:"blur(5px)", "-webkit-filter":"blur(4px)"})
-  var tw4 = TweenLite.to(html_body, 0.5, {overflow:"hidden"})
-  tl = new TimelineLite().add([tw1,tw2,tw3,tw4], 'sequence');
-}
 
 
 
