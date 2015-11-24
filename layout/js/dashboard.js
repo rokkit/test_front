790f17fc6405561abe47771bb9e7bea07c2c4d9e
Maxim Indykov <indmaksim@gmail.com>
2015-11-24 16:21:02 +0300

    Fix animations

--- layout/js/dashboard.js
+++ layout/js/dashboard.js
@@ -12,8 +12,24 @@ $(function() {
   		animateForm('reserv_form');
   	});
 
+    $('#reserv_form').submit(function(e){
+      e.preventDefault();
+      var form_reserv = document.getElementById('reserv_form');
+      TweenLite.to(form_reserv, 1, {left:"1860px"});
+      animateFormSuccess('reserv_succes_form');
+    });
 
-
+    $('#reserv_succes_form').submit(function(e){
+      var form_success = document.getElementById('reserv_succes_form');
+      var html_body3 = document.getElementById("html_body")
+      var color_overlay3 = document.getElementById("color_overlay")
+      var main3 = document.getElementById('main_content')
+      TweenLite.to(form_success, 1, {right:"-1260px"});
+      TweenLite.to(html_body3, 1, {overflow:"auto"})
+      TweenLite.to(color_overlay3, 1, {opacity:"0", 'pointer-events': 'none'});
+      TweenLite.to(main3, 1, {filter:"blur(0px)", "-webkit-filter":"blur(0px)", transform:"scale(1, 1)"});
+      e.preventDefault();
+    });
 
 
     //Открытие всех достижений
@@ -42,65 +58,50 @@ $(function() {
 })
 
 function animateFormSuccess(el) {
-  var form = document.getElementById(el)
-  var html_body = document.getElementById("html_body")
-  var color_overlay = document.getElementById("color_overlay")
-  var main = document.getElementById('main_content')
-  var wrapper =document.getElementById('wrapper_login')
+  var form2 = document.getElementById(el)
+  var html_body2 = document.getElementById("html_body")
+  var color_overlay2 = document.getElementById("color_overlay")
+  var main2 = document.getElementById('main_content')
+  var wrapper2 =document.getElementById('wrapper_login')
   var tl = null
 
-  var tw1 = TweenLite.to(form, 1, {right:"0px", onComplete: function() {
+  var tw1 = TweenLite.to(form2, 1, {right:"0px", onComplete: function() {
     $('.popup').click(function(event){
       event.stopPropagation();
     });
     $('body').on('click', function(e) {
       tl.reverse()
     });
-    $('#reserv_succes_form').submit(function(e){
-      $('#reserv_succes_form').off()
-      var form = document.getElementById('reserv_succes_form');
-      TweenLite.to(form, 1, {right:"-1260px"});
-      TweenLite.to(html_body, 1, {overflow:"auto"})
-      TweenLite.to(color_overlay, 1, {opacity:"0", 'pointer-events': 'none'});
-      TweenLite.to(main_content, 1, {filter:"blur(0px)", "-webkit-filter":"blur(0px)", transform:"scale(1, 1)"});
-      e.preventDefault();
-    });
+
   }})
-  var tw2 = TweenLite.to(color_overlay, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
-  var tw3 = TweenLite.to(main_content, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
-  var tw4 = TweenLite.to(html_body, 1, {overflow:"hidden"})
-  var tw5 = TweenLite.to(form, 1, {'pointer-events':"auto"})
+  var tw2 = TweenLite.to(color_overlay2, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
+  var tw3 = TweenLite.to(main2, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
+  var tw4 = TweenLite.to(html_body2, 1, {overflow:"hidden"})
+  var tw5 = TweenLite.to(form2, 1, {'pointer-events':"auto"})
   tl = new TimelineLite().add([tw1,tw2,tw3,tw4,tw5], 'sequence');
 }
 
 //Анимация формы бронирования
 function animateForm(el) {
-  var form = document.getElementById(el)
-  var html_body = document.getElementById("html_body")
-  var color_overlay = document.getElementById("color_overlay")
-  var main = document.getElementById('main_content')
-  var wrapper = document.getElementById('wrapper_login')
+  var form1 = document.getElementById(el)
+  var html_body1 = document.getElementById("html_body")
+  var color_overlay1 = document.getElementById("color_overlay")
+  var main1 = document.getElementById('main_content')
+  var wrapper1 = document.getElementById('wrapper_login')
   var tl = null;
-  var tw1 = TweenLite.to(form, 1, {left:"160px", onComplete: function() {
+  var tw1 = TweenLite.to(form1, 1, {left:"160px", onComplete: function() {
     $('.popup').click(function(event){
       event.stopPropagation();
     });
-    // $('body').on('click', function(e) {
-    //   tl.reverse()
-    // });
-    $('#reserv_form').submit(function(e){
-      $('#reserv_form').off()
-      var form = document.getElementById('reserv_form');
-      TweenLite.to(form, 1, {left:"1860px"});
-
-      animateFormSuccess('reserv_succes_form');
-      e.preventDefault();
+    $('body').on('click', function(e) {
+      tl.reverse()
     });
+
   }})
-  var tw2 = TweenLite.to(color_overlay, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
-  var tw3 = TweenLite.to(main_content, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
-  var tw4 = TweenLite.to(html_body, 1, {overflow:"hidden"})
-  var tw5 = TweenLite.to(form, 1, {'pointer-events':"auto"})
+  var tw2 = TweenLite.to(color_overlay1, 1, {opacity:"0.8", "-webkit-opacity":"1", 'pointer-events':"auto"})
+  var tw3 = TweenLite.to(main1, 1, {filter:"blur(5px)", "-webkit-filter":"blur(4px)", transform:"scale(0.95, 0.95)"})
+  var tw4 = TweenLite.to(html_body1, 1, {overflow:"hidden"})
+  var tw5 = TweenLite.to(form1, 1, {'pointer-events':"auto"})
   tl = new TimelineLite().add([tw1,tw2,tw3,tw4,tw5], 'sequence');
 }
 
