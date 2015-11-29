677e64f933ad5e6e79dec09e0227337d48b7cc52
Your Name <you@example.com>
2015-11-29 16:23:44 +0300

    Fix

--- layout/js/dashboard.js
+++ layout/js/dashboard.js
@@ -188,3 +188,21 @@ $(function() {
         })
     })
 })
+
+// Центрирование попапов
+
+jQuery.fn.center = function(parent) {
+    if (parent) {
+        parent = this.parent();
+    } else {
+        parent = window;
+    }
+    this.css({
+        "position": "absolute",
+        "top": ((($(parent).height() - this.outerHeight()) / 2) + $(parent).scrollTop() + "px"),
+        "left": ((($(parent).width() - this.outerWidth()) / 2) + $(parent).scrollLeft() + "px")
+    });
+return this;
+}
+$("div.target:nth-child(1)").center(true);
+$("div.target:nth-child(2)").center(false);
