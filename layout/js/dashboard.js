3a1ce6a4c62b0f958c1686eeeea90dbb64bfaaf7
Maxim Indykov <indmaksim@gmail.com>
2015-12-09 16:29:03 +0300

    Fix

--- layout/js/dashboard.js
+++ layout/js/dashboard.js
@@ -165,16 +165,6 @@ $(function() {
 
 
 
-    $('.username h1').click(function(){
-      fx.do(['background', 'editProfile'], bodyClick, bodyClickOff);
-    });
-    $('#edit_profile_btn').click(function(){
-      fx.do(['background', 'editProfile'], bodyClick, bodyClickOff);
-    });
-
-    $('#edit-profile a').click(function(){
-      fx.back();
-    });
 
     $('#logout_btn').click(function() {
       localStorage.removeItem('currentUser')
@@ -182,32 +172,6 @@ $(function() {
       document.location.href = '/pages_index.html'
     });
 
-    window.hostUrl = 'http://176.112.194.149:81'
-    // window.hostUrlLocal = 'http://localhost:3000'
-    var $profile_wrapper = $('#edit-profile-wrapper')
-    $profile_wrapper.find('input[name="name"]').val(currentUser.name)
-    $profile_wrapper.find('input[name="city"]').val(currentUser.city)
-    $profile_wrapper.find('input[name="employe"]').val(currentUser.employe)
-    $profile_wrapper.find('input[name="work_company"]').val(currentUser.work_company)
-    $profile_wrapper.find('input[name="hobby"]').val(currentUser.hobby)
-    $profile_wrapper.find('input[name="phone"]').val('+'+currentUser.phone)
-    $profile_wrapper.find('input[name="email"]').val(currentUser.email)
-
-    $('form.edit_profile_form').on('submit', function(e) {
-      e.preventDefault()
-
-      var data = $(this).serialize() + '&auth_token='+currentUser.auth_token
-      $.ajax({url: hostUrl + '/api/v1/users/' + currentUser.id, data: data, success: function(user) {
-        window.currentUser = user
-        localStorage.setItem('currentUser', JSON.stringify(user))
-        $('section.username h1').text(currentUser.name)
-    		$('#login_btn').text(currentUser.name)
-        if(currentUser.city) {
-          $('#city_user span').text(currentUser.city)
-        }
-        fx.back();
-      }, type: 'PUT'});
-    })
 });
 
 $(function(){
--- layout/js/profile.js
+++ layout/js/profile.js
@@ -1,1 +1,49 @@
+window.hostUrl = 'http://176.112.194.149:81'
+$(function() {
+  $('.username h1').click(function(){
+    fx.do(['background', 'editProfile'], bodyClick, bodyClickOff);
+  });
+  $('#edit_profile_btn').click(function(){
+    fx.do(['background', 'editProfile'], bodyClick, bodyClickOff);
+  });
 
+  $('#edit-profile a').click(function(){
+    fx.back();
+  });
+  var $profile_wrapper = $('#edit-profile-wrapper')
+  $profile_wrapper.find('input[name="name"]').val(currentUser.name)
+  $profile_wrapper.find('input[name="city"]').val(currentUser.city)
+  $profile_wrapper.find('input[name="employe"]').val(currentUser.employe)
+  $profile_wrapper.find('input[name="work_company"]').val(currentUser.work_company)
+  $profile_wrapper.find('input[name="hobby"]').val(currentUser.hobby)
+  $profile_wrapper.find('input[name="phone"]').val('+'+currentUser.phone)
+  $profile_wrapper.find('input[name="email"]').val(currentUser.email)
+
+  $('form.edit_profile_form').on('submit', function(e) {
+    e.preventDefault()
+
+    var data = $(this).serialize() + '&auth_token='+currentUser.auth_token
+    $.ajax({url: hostUrl + '/api/v1/users/' + currentUser.id, data: data, success: function(user) {
+      window.currentUser = user
+      localStorage.setItem('currentUser', JSON.stringify(user))
+      $('section.username h1').text(currentUser.name)
+      $('#login_btn').text(currentUser.name)
+      if(currentUser.city) {
+        $('#city_user span').text(currentUser.city)
+      }
+      fx.back();
+    }, type: 'PUT'});
+  })
+})
+
+function bodyClick(e){
+  $('body').on('click', function(e) {
+    TweenLite.to('section.error_tooltip', 1, {opacity: 0});
+    fx.back();
+  });
+}
+
+function bodyClickOff(){
+  $('body').off('click');
+  $('#reserv_succes_form').css('right', '1600');
+}
