4ac5fd4e66d3071a5891747de76c04f14c55acd2
Maxim Indykov <indmaksim@gmail.com>
2015-12-17 22:00:17 +0300

    Временно закоменчены карточки

--- layout/js/header.js
+++ layout/js/header.js
@@ -6,46 +6,7 @@ $(function(){
   });
 });
 
-$(function(){
-  var el;
-  if($('.lounges_block_5 .map_wrapper').length > 0){
-    el = $('.lounges_block_5 .map_wrapper');
-  }
-  if($('#liberty_block5 .map_wrapper').length > 0){
-    el = $('#liberty_block5 .map_wrapper');
-  }
-  // if(typeof google !== 'undefined' && $('.lounges_block_5 .map_wrapper').length > 0){
-  if(typeof google !== 'undefined' && el.length > 0){
-    (function(){
-      var lat = el.data('map-lat');
-      var lng = el.data('map-lng');
-      var mapProp = {
-          center:new google.maps.LatLng(lat, lng),
-          zoom:15,
-          scrollwheel: false,
-          navigationControl: false,
-          mapTypeControl: false,
-          scaleControl: true,
-          draggable: true,
-          mapTypeId:google.maps.MapTypeId.ROADMAP,
-          disableDefaultUI: true,
-          styles: [
-            {"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},
-            {"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}
-          ]
-        };
 
-        var map = new google.maps.Map(el[0], mapProp);
-
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, lng),
-            map: map
-        });
-
-
-    })();
-  }
-});
 
 function bodyClick(e){
   $('body').on('click', function(e) {
@@ -127,15 +88,21 @@ $(function() {
   });
 
   $('#reg_block5').on('click', function(){
-    animateSignup();
-    animateBG();
-    bodyClick();
+    if (!currentUser) {
+      $('#wrapper_signup').css('pointer-events', 'auto');
+      fx.do(['errorTooltip', 'signup', 'background'], bodyClick, bodyClickOff);
+    } else {
+      document.location.href = '/tech_preloader.html?redirect=profile'
+    }
   });
 
   $('#reserve_table_btn').on('click', function() {
-    animateSignup();
-    animateBG();
-    bodyClick();
+    if (!currentUser) {
+      $('#wrapper_signup').css('pointer-events', 'auto');
+      fx.do(['errorTooltip', 'signup', 'background'], bodyClick, bodyClickOff);
+    } else {
+      document.location.href = '/tech_preloader.html?redirect=profile'
+    }
   });
 
   $('#recover_btn').on('click', function() {
