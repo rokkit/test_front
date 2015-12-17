745faf46a9afc9db676b5d50551ef7939effcc6f
SooFoo <do@cpdbbk.com>
2015-12-17 18:20:10 +0300

    fix conflict

--- layout/js/header.js
+++ layout/js/header.js
@@ -7,10 +7,18 @@ $(function(){
 });
 
 $(function(){
-  if(typeof google !== 'undefined' && $('.lounges_block_5 .map_wrapper').length > 0){
+  var el;
+  if($('.lounges_block_5 .map_wrapper').length > 0){
+    el = $('.lounges_block_5 .map_wrapper');
+  }
+  if($('#liberty_block5 .map_wrapper').length > 0){
+    el = $('#liberty_block5 .map_wrapper');
+  }
+  // if(typeof google !== 'undefined' && $('.lounges_block_5 .map_wrapper').length > 0){
+  if(typeof google !== 'undefined' && el.length > 0){
     (function(){
-      var lat = $('.lounges_block_5 .map_wrapper').data('map-lat');
-      var lng = $('.lounges_block_5 .map_wrapper').data('map-lng');
+      var lat = el.data('map-lat');
+      var lng = el.data('map-lng');
       var mapProp = {
           center:new google.maps.LatLng(lat, lng),
           zoom:15,
@@ -27,7 +35,7 @@ $(function(){
           ]
         };
 
-        var map = new google.maps.Map($('.lounges_block_5 .map_wrapper')[0], mapProp);
+        var map = new google.maps.Map(el[0], mapProp);
 
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, lng),
