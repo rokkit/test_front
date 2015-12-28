ef95f95636fd5893814979371151815979c90e65
Maxim Indykov <indmaksim@gmail.com>
2015-12-28 17:43:31 +0300

    Add budjgt

--- layout/js/pages_franchise.js
+++ layout/js/pages_franchise.js
@@ -47,6 +47,15 @@ $(function(){
     $('html, body').animate({
         scrollTop: $("#frn_b2").offset().top
     }, 900);
-});
+  });
+
+  $("#total_payment_input").keypress(function (e) {
+   //if the letter is not digit then display error and don't type anything
+   if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      //display error message
+     return false;
+  }
+ });
+
 
 });
