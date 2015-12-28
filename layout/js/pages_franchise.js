d88940102f1dcd922fff56459f96432ee39bd94c
Maxim Indykov <indmaksim@gmail.com>
2015-12-28 13:37:39 +0300

    Добавлено убирание формы

--- layout/js/pages_franchise.js
+++ layout/js/pages_franchise.js
@@ -6,8 +6,14 @@ $(function(){
   franchise_popup.from("#frn_b6", 0.1, {autoAlpha:0});
   franchise_popup.to("#frn_b6", 0.1, {autoAlpha:1});
 
-  franchise_popup.to(".wrapper_form", 1, {top:"160px", ease: Power4.easeOut});
-  franchise_popup.to("#html_body", 0.1, {overflow:"hidden"});
+  franchise_popup.to(".wrapper_form", 1, {top:"160px", ease: Power4.easeOut, onComplete: function() {
+    $('body').on('click', function(e) {
+      console.log('body click')
+      franchise_popup.reverse();
+      $('body').off('click')
+    });
+  }});
+  franchise_popup.to("#html_body", 1, {overflow:"hidden"});
 
   $(".open_franchise_popup").click(function() {
     franchise_popup.play();
@@ -16,4 +22,5 @@ $(function(){
   // $(".cross_white_popup").click(function() {
   //   franchise_popup.reverse();
   //   });
+
 })
