67fa383c4e90b81f30f5a7e301f465d610e63805
Burakov <you@example.com>
2015-12-28 12:51:07 +0300

    Добавлена страница для франшайзеров

---
+++ layout/js/pages_franchise.js
@@ -0,0 +1,21 @@
+// Открытие попапа
+$(function(){
+
+  var franchise_popup = new TimelineMax({paused:true});
+
+  franchise_popup.from("#frn_b6", 0.1, {autoAlpha:0, background:"none"});
+  franchise_popup.to("#frn_b6", 0.1, {autoAlpha:1, background:"rgba(0,0,0,0.9"});
+
+  franchise_popup.to(".wrapper_form", 1, {top:"160px", ease: Power4.easeOut});
+  franchise_popup.to("#html_body", 0.1, {overflow:"hidden"});
+
+  $(".open_franchise_popup").click(function() {
+    franchise_popup.play();
+  });
+
+  // $(".cross_white_popup").click(function() {
+  //   franchise_popup.reverse();
+  //   });
+})
+
+
