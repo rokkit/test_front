698ff11c72942db611ef697e26e7f1cb69264680
Burakov <you@example.com>
2015-12-28 13:53:43 +0300

    Поправлена анимация попапа страницы для франшайзеров

--- layout/js/pages_franchise.js
+++ layout/js/pages_franchise.js
@@ -3,8 +3,14 @@ $(function(){
 
   var franchise_popup = new TimelineMax({paused:true});
 
-  franchise_popup.from("#frn_b6", 0.1, {autoAlpha:0});
-  franchise_popup.to("#frn_b6", 0.1, {autoAlpha:1});
+  franchise_popup.from("#frn_b6", 0.01, {autoAlpha:0});
+  franchise_popup.to("#frn_b6", 0.01, {autoAlpha:1});
+
+  franchise_popup.to("#html_body", 0.01, {overflow:"hidden"});
+
+  $(".open_franchise_popup").click(function() {
+    franchise_popup.play();
+  });
 
   franchise_popup.to(".wrapper_form", 1, {top:"160px", ease: Power4.easeOut, onComplete: function() {
     $('body').on('click', function(e) {
@@ -16,11 +22,6 @@ $(function(){
      e.stopPropagation();
     });
   }});
-  franchise_popup.to("#html_body", 1, {overflow:"hidden"});
-
-  $(".open_franchise_popup").click(function() {
-    franchise_popup.play();
-  });
 
   // $(".cross_white_popup").click(function() {
   //   franchise_popup.reverse();
